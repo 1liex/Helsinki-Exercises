71840bb68c975d4253ca162c59aa66895a4e74a7
yasir alshekahi <yaaserjoker1@gmail.com>
2026-01-14 01:36:19 +0300

    finish form unicafe exercise

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,13 +1,91 @@
-import { useState } from 'react'
+import { useState } from "react";
 
+const Btns = ({ good, setGood, neutral, setNeutral, bad, setBad }) => {
+  return (
+    <>
+      <button
+        onClick={() => {
+          setGood(good + 1);
+        }}
+      >
+        good
+      </button>
+      <button
+        onClick={() => {
+          setNeutral(neutral + 1);
+        }}
+      >
+        neutral
+      </button>
+      <button
+        onClick={() => {
+          setBad(bad + 1);
+        }}
+      >
+        bad
+      </button>
+    </>
+  );
+};
 
-function App() {
+const Statistics = ({ text, value }) => {
+  let percent = "";
+
+  if (text === "positive") {
+    percent = "%";
+  }
+  return (
+    <>
+      <tr>
+        <td style={{ padding: "0 15px 0 0" }}>{text}</td>
+        <td>
+          {value}
+          {percent}
+        </td>
+      </tr>
+    </>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const allFeed = good + neutral + bad;
+  const average = allFeed === 0 ? 0 : (good - bad) / allFeed;
+  const positive = allFeed === 0 ? 0 : (good / allFeed) * 100;
 
   return (
     <>
-     
-    </>
-  )
-}
+      <h1>give feedback</h1>
+      <Btns
+        good={good}
+        setGood={setGood}
+        neutral={neutral}
+        setNeutral={setNeutral}
+        bad={bad}
+        setBad={setBad}
+        allFeed={allFeed}
+      />
+      <div style={allFeed ? { display: "block" } : { display: "none" }}>
+        <table>
+          <tbody>
+            <Statistics text={"good"} value={good} />
+            <Statistics text={"neutral"} value={neutral} />
+            <Statistics text={"bad"} value={bad} />
+            <Statistics text={"all"} value={allFeed} />
+            <Statistics text={"average"} value={average} />
+            <Statistics text={"positive"} value={positive} />
+          </tbody>
+        </table>
+      </div>
 
-export default App
+      <p style={!allFeed ? { display: "block" } : { display: "none" }}>
+        No feedback given
+      </p>
+    </>
+  );
+};
+
+export default App;
