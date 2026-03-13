7c4278dee082ad85f7a06002101fa22fcc1d745f
yasir alshekahi <yaaserjoker1@gmail.com>
2026-03-13 07:55:35 +0300

    finished a part from part2

--- part2/ThePhonebook/src/App.jsx
+++ part2/ThePhonebook/src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Filter from "./modules/Filter";
 import AddingNewPeople from "./modules/AddingNewPeople";
 import Persons from "./modules/Persons";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [phoneNum, setPhoneNum] = useState("");
   const [newName, setNewName] = useState("");
   const [showPersonInfo, setShowPersonInfo] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
 
   return (
     <div>
